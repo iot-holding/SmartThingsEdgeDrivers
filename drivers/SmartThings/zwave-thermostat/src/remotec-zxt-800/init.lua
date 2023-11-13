0583fee6bc4292d192edb60e6162b87bb419cda4
Niels Roche <nroche.z-wave@outlook.de>
2023-11-13 13:59:35 +0100

    remotec-zxt-800: some fixes and refactorings

--- drivers/SmartThings/zwave-thermostat/src/remotec-zxt-800/init.lua
+++ drivers/SmartThings/zwave-thermostat/src/remotec-zxt-800/init.lua
@@ -15,21 +15,36 @@
 local preferencesMap = require "remotec-zxt-800.preferences"
 local st_device = require "st.device"
 local capabilities = require "st.capabilities"
+
 --- @type st.zwave.CommandClass
 local cc = require "st.zwave.CommandClass"
 --- @type st.zwave.CommandClass.Configuration
 local Configuration = (require "st.zwave.CommandClass.Configuration")({version=1})
 --- @type remotec-zxt-800.SimpleAVControl
 local AVControl = (require "remotec-zxt-800.SimpleAVControl")({version=3})
+--- @type st.zwave.CommandClass.SensorMultilevel
 local SensorMultiLevel = (require "st.zwave.CommandClass.SensorMultilevel")({version = 2})
+--- @type st.zwave.CommandClass.ThermostatFanMode
+local ThermostatFanMode = (require "st.zwave.CommandClass.ThermostatFanMode")({ version = 3 })
+--- @type st.zwave.CommandClass.ThermostatMode
+local ThermostatMode = (require "st.zwave.CommandClass.ThermostatMode")({ version = 2 })
+
 local log = require "log"
 local utils = require "st.utils"
 
 local ENDPOINTS = {
   parent = 0,
-  child1 = 1,
-  child2 = 2,
-  child3 = 3
+  children = 3
+}
+
+local supported_modes = {
+  capabilities.thermostatMode.thermostatMode.off.NAME,
+  capabilities.thermostatMode.thermostatMode.heat.NAME,
+  capabilities.thermostatMode.thermostatMode.cool.NAME,
+  capabilities.thermostatMode.thermostatMode.auto.NAME,
+  capabilities.thermostatMode.thermostatMode.resume.NAME,
+  capabilities.thermostatMode.thermostatMode.fanonly.NAME,
+  capabilities.thermostatMode.thermostatMode.dryair.NAME
 }
 
 local REMOTEC_FINGERPRINTS = {
@@ -41,15 +56,10 @@ local function can_handle_remotec(opts, driver, device, ...)
             return true
         end
     end
-  
+
     return false
   end
 
-
-  local function component_to_endpoint(device, component)
-    log.debug(ENDPOINTS)
-    return { ENDPOINTS.parent }
-end
 local function info_changed(driver, device, event, args)
     preferencesMap.update_preferences(driver, device, args)
   end
@@ -67,9 +77,11 @@ local function find_child(parent, ep_id)
   end
 
 local function do_refresh(driver, device, command)
-    local component = command and command.component and command.component or "main"
-    device:send_to_component(SensorMultiLevel:Get({}), component)
-  end
+    device:refresh()
+    device:send(SensorMultiLevel:Get({}))
+    device:send(ThermostatMode:SupportedGet({}))
+    device:send(ThermostatFanMode:SupportedGet({}))
+end
 
 local function component_to_endpoint(device, component)
     return { ENDPOINTS.parent }
@@ -77,62 +89,39 @@ local function component_to_endpoint(device, component)
 
 local function device_added(driver, device, event)
     if device.network_type == st_device.NETWORK_TYPE_ZWAVE and
-      not (device.child_ids and utils.table_size(device.child_ids) ~= 0) and
-      find_child(device, ENDPOINTS.child1) == nil then
-  
-      local name = string.format("%s %s", "ZXT 800 ", "EP 1")
-      local metadata = {
-        type = "EDGE_CHILD",
-        label = name,
-        profile = "remotec-zxt-800-child",
-        parent_device_id = device.id,
-        parent_assigned_child_key = string.format("%02X", ENDPOINTS.child1),
-        vendor_provided_label = name,
-      }
-      driver:try_create_device(metadata)
-    end
+      not (device.child_ids and utils.table_size(device.child_ids) ~= 0) then
 
-    if device.network_type == st_device.NETWORK_TYPE_ZWAVE and
-      not (device.child_ids and utils.table_size(device.child_ids) ~= 0) and
-      find_child(device, ENDPOINTS.child2) == nil then
-  
-      local name = string.format("%s %s", "ZXT 800 ", "EP 2")
-      local metadata = {
-        type = "EDGE_CHILD",
-        label = name,
-        profile = "remotec-zxt-800-child",
-        parent_device_id = device.id,
-        parent_assigned_child_key = string.format("%02X", ENDPOINTS.child2),
-        vendor_provided_label = name,
-      }
-      driver:try_create_device(metadata)
-    end
+      for i=1, ENDPOINTS.children do
+        if find_child(device, i) == nil then
 
-    if device.network_type == st_device.NETWORK_TYPE_ZWAVE and
-      not (device.child_ids and utils.table_size(device.child_ids) ~= 0) and
-      find_child(device, ENDPOINTS.child3) == nil then
-  
-      local name = string.format("%s %s", "ZXT 800 ", "EP 3")
-      local metadata = {
-        type = "EDGE_CHILD",
-        label = name,
-        profile = "remotec-zxt-800-child",
-        parent_device_id = device.id,
-        parent_assigned_child_key = string.format("%02X", ENDPOINTS.child3),
-        vendor_provided_label = name,
-      }
-      driver:try_create_device(metadata)
+        local name = string.format("%s %s", "ZXT 800 ", "EP " .. i)
+        local metadata = {
+          type = "EDGE_CHILD",
+          label = name,
+          profile = "remotec-zxt-800-child",
+          parent_device_id = device.id,
+          parent_assigned_child_key = string.format("%02X", i),
+          vendor_provided_label = name,
+        }
+        driver:try_create_device(metadata)
+        end
+      device:emit_event(capabilities.thermostatMode.supportedThermostatModes(supported_modes,
+        { visibility = { displayed = false } }))
+      end
     end
+    device:emit_event(capabilities.thermostatMode.supportedThermostatModes(supported_modes,
+    { visibility = { displayed = false } }))
     do_refresh(driver, device)
   end
 
 
 local function device_init(driver, device, event)
-    if device.network_type == st_device.NETWORK_TYPE_ZWAVE then
-      device:set_find_child(find_child)
-      device:set_component_to_endpoint_fn(component_to_endpoint)
-    end
+  if device.network_type == st_device.NETWORK_TYPE_ZWAVE then
+    device:set_find_child(find_child)
+    device:set_component_to_endpoint_fn(component_to_endpoint)
   end
+end
+
 local remotec_controller = {
     NAME = "remotec-zxt-800",
     zwave_handlers = {
